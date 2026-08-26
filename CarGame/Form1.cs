2b3f7d657921f19267107a3fbd9959aa0ddf1682
renzdelos_santos55 <renzdsts5@gmail.com>
2026-08-26 18:28:58 +0800

    Add scrolling road background to CarGame Form

--- CarGame/Form1.cs
+++ CarGame/Form1.cs
@@ -12,6 +12,13 @@ namespace CarGame
 {
     public partial class Form1 : Form
     {
+        //ROAD
+        private Timer timerRoad;
+        private Image roadImage;
+        private int roadWidth;
+        private int roadHeight;
+        private float roadY;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,6 +29,7 @@ namespace CarGame
         {
             InitilizeWindow();
             RegisterEvets();
+            InitializeRoad();
         }
 
         private void InitilizeWindow()
@@ -34,6 +42,20 @@ namespace CarGame
             KeyPreview = true;
         }
 
+        private void InitializeRoad()
+        {
+            roadImage = Properties.Resources.road;
+            roadWidth = ClientSize.Width;
+            roadHeight = ClientSize.Height;
+
+            timerRoad = new Timer();
+            timerRoad.Interval = 30;
+            timerRoad.Tick += TimerRoad_Tick;
+            timerRoad.Start();
+        }
+
+
+
         private void RegisterEvets()
         {
             Paint += Form1_Paint;
@@ -43,24 +65,54 @@ namespace CarGame
             KeyDown += Form1_KeyDown;
         }
 
+
+
+
+        private void TimerRoad_Tick(object sender, EventArgs e)
+        {
+            roadY += 5;
+
+            if (roadY >= roadHeight)
+                roadY -= roadHeight;
+
+            if (roadY < 0)
+                roadY += roadHeight;
+
+            Invalidate();
+
+        }
+
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
+
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
+
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
+
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
+
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            DrawRoad(e.Graphics);
         }
+
+
+        private void DrawRoad(Graphics g)
+        {
+            g.DrawImage(roadImage, 0, roadY, roadWidth, roadHeight);
+            g.DrawImage(roadImage, 0, roadY - roadHeight, roadWidth, roadHeight);
+        }
+
     }
 }
